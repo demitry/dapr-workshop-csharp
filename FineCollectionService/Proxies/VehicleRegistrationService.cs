d88c4f531eb027473220e210a9acd5de249eb601
DESKTOP-V9JHRM2\Dmitry <dpoluektov@gmail.com>
2025-06-27 20:28:58 +0300

    Assignment 2 - service-to-service invocation

--- FineCollectionService/Proxies/VehicleRegistrationService.cs
+++ FineCollectionService/Proxies/VehicleRegistrationService.cs
@@ -11,7 +11,12 @@ public class VehicleRegistrationService
 
     public async Task<VehicleInfo> GetVehicleInfo(string licenseNumber)
     {
-        return await _httpClient.GetFromJsonAsync<VehicleInfo>(
-            $"http://localhost:6002/vehicleinfo/{licenseNumber}");
+        // return await _httpClient.GetFromJsonAsync<VehicleInfo>(
+        //     $"http://localhost:6002/vehicleinfo/{licenseNumber}");
+
+        // return await _httpClient.GetFromJsonAsync<VehicleInfo>(
+        //     $"http://localhost:3601/v1.0/invoke/vehicleregistrationservice/method/vehicleinfo/{licenseNumber}");
+
+        return await _httpClient.GetFromJsonAsync<VehicleInfo>($"/vehicleinfo/{licenseNumber}");
     }
 }
